c2661f7b860055c68194491c49a820ebad188e64
IsaiahGz <isaiahgala99@gmail.com>
2023-05-16 16:49:20 -0400

    Styling changes and added log out feature

--- client/src/App.js
+++ client/src/App.js
@@ -14,47 +14,47 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 const httpLink = createHttpLink({
-	uri: '/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
-	const token = localStorage.getItem('id_token');
-	return {
-		headers: {
-			...headers,
-			authorization: token ? `Bearer ${token}` : '',
-		},
-	};
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 const client = new ApolloClient({
-	link: authLink.concat(httpLink),
-	cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
-	return (
-		<ApolloProvider client={client}>
-			<Router>
-				<div className='relative min-h-screen'>
-					<Header />
-					<div className= "pb-28">
-						<Routes>
-							<Route path='/' element={<Home />} />
-							<Route path='/testimonials' element={<Testimonials />} />
-							<Route path='/about' element={<About />} />
-							<Route path='/services' element={<Acne />} />
-							<Route path='/contact' element={<Contact />} />
-							<Route path='/shop' element={<Shop />} />
-							<Route path='/login' element={<LoginPage />} />
-							<Route path='/admin' element={<Admin />} />
-						</Routes>
-					</div>
-					<Footer />
-				</div>
-			</Router>
-		</ApolloProvider>
-	);
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className='relative min-h-screen'>
+          <Header />
+          <div className='pb-36'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/testimonials' element={<Testimonials />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/services' element={<Acne />} />
+              <Route path='/contact' element={<Contact />} />
+              <Route path='/shop' element={<Shop />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/admin' element={<Admin />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
